a5acfd41917bdd9f277499e4b3a38b9d26118af4
coramarcet <cjmarcet@stu.naperville203.org>
2026-06-28 15:28:56 -0500

    Frontend
    React and Vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import ribbit from './assets/ribbit.jpg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>digikitty Web App</h1>
+      <p>Welcome!</p>
+      <img src={ribbit} width="300"></img>
+    </div>
+  )
+}
+
+export default App
